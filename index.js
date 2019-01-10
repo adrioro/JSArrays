f7700aed82af6f97812b1a2e93ab3ef9f4fc7c20
adrioro <artistsvillage@me.com>
2019-01-10 12:48:30 -0500

    fourth commit multi-dimentional array

--- index.js
+++ index.js
@@ -19,5 +19,17 @@ document.write("<br>" + "It looks like" + SameArray[2] + " and " + SameArray[1] 
 
 var NextArray = ["Julia", "Joe", "Carla"];
 
-document.write("<br>" + NextArray[1] + " is the son of " + NextArray[2])
-''
+document.write("<br>" + NextArray[1] + " is the son of " + NextArray[2]);
+
+//MULTI-DIMENTIONAL ARRAY//
+
+var MultiArray = [
+  ["Ana", "Peter", "Jane"],
+  ["John", "Tom", "Dan"]
+];
+
+
+document.write("<br>" + MultiArray[0][0] + " is the mother of " + MultiArray[1][0]);
+document.write("<br>" + MultiArray[0][2] + " is the wife of " + MultiArray[1][0]);
+document.write("<br>" + MultiArray[0][1] + " and " + MultiArray[1][1] +
+  " are in a relationship. ");
