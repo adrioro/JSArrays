fb85ca1eea7439b768916ca92bfe0c158fd9235a
adrioro <artistsvillage@me.com>
2019-01-10 11:40:38 -0500

    third commit literal array

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ MyArray[0] = "Bob";
 MyArray[1] = "Bill";
 MyArray[2] = "Joe";
 
-//or//
+//alert(MyArray[2] + " is a nice guy!");
+//OR//
+
+document.write(MyArray[0] + " and " + MyArray[2] + " are friends.");
+
+//or CONDENSING THE ARRAY//
+
 var SameArray = new Array("Ana", "Suzanne", "Mary");
 
-document.write("It looks like" + SameArray[2] + " and " + SameArray[1] + " get alone very well.");
+document.write("<br>" + "It looks like" + SameArray[2] + " and " + SameArray[1] + " get alone very well.");
 
-//alert(MyArray[2] + " is a nice guy!");
+//LITERAL ARRAYS
 
-document.write("<br> " + MyArray[0] + " and " + MyArray[2] + " are friends.");
+var NextArray = ["Julia", "Joe", "Carla"];
+
+document.write("<br>" + NextArray[1] + " is the son of " + NextArray[2])
+''
