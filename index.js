0adb40a59888a8975b40233d17f1b3efb8060c9a
adrioro <artistsvillage@me.com>
2019-01-10 11:12:11 -0500

    second commit second array and line break

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ MyArray[0] = "Bob";
 MyArray[1] = "Bill";
 MyArray[2] = "Joe";
 
-alert(MyArray[2] + " is a nice guy!");
+//or//
+var SameArray = new Array("Ana", "Suzanne", "Mary");
 
-document.write(MyArray[0] + " and " + MyArray[2] + " are friends.");
+document.write("It looks like" + SameArray[2] + " and " + SameArray[1] + " get alone very well.");
+
+//alert(MyArray[2] + " is a nice guy!");
+
+document.write("<br> " + MyArray[0] + " and " + MyArray[2] + " are friends.");
